5b85fa152aa00ce5750d7affbefecf3b42906fa4
Damian Flynn <technology@damianflynn.com>
2018-04-19 20:43:56 +0000

    BOT Framework Code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=azbootcampdubbot-Web-Deploy.pubxml -p:Password=i1twkDP939NvTScA61rrhi9hKa8ZCJGcQZrjJvWJTGck4dW39bGJ8KPqqrs6
+

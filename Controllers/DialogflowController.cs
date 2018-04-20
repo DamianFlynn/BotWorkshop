f221c73691c1acc5293d94eee04f0d7e81b8d24d
Damian Flynn <technology@damianflynn.com>
2018-04-20 22:39:25 +0000

    Added support for Dialogflow, using the v1 API.AI.SDK and and a controller for this endpoint

---
+++ Controllers/DialogflowController.cs
@@ -0,0 +1,31 @@
+﻿using ApiAiSDK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleEchoBot.Controllers
+{
+    public class DialogflowController : ApiController
+    {
+        public dynamic Post(AIResponse aiResponse)
+        {
+
+            var response = new
+            {
+                speech = "Hello Dialogflow",
+                text = "Hello Dialogflow"
+
+            };
+
+            return response;
+        }
+
+        public string Get()
+        {
+            return "Hello Dialogflow!";
+        }
+    }
+}

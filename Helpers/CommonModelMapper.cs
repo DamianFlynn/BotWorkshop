1bb5f176c2f786bd8a9c9b178c262698d2dee503
Damian Flynn <technology@damianflynn.com>
2018-04-20 22:55:07 +0000

    Added the Common Model, to map with the Json we are using for the response and request

---
+++ Helpers/CommonModelMapper.cs
@@ -0,0 +1,47 @@
+﻿using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using SimpleEchoBot.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot.Helpers
+{
+    public class CommonModelMapper
+    {
+
+        // Mapping Alexa SDK to Common Model
+
+        internal static CommonModel AlexaToCommonModel(SkillRequest skillRequest)
+        {
+            var commonModel = new CommonModel()
+            {
+                Id = skillRequest.Request.RequestId
+            };
+
+            var requestType = skillRequest.GetRequestType();
+
+            if (requestType == typeof(IntentRequest))
+            {
+                var intentRequest = skillRequest.Request as IntentRequest;
+                commonModel.Request.Intent = intentRequest.Intent.Name;
+
+                if (intentRequest.Intent.Slots != null)
+                    commonModel.Request.Parameters = intentRequest.Intent.Slots.ToList()
+                        .ConvertAll(s => new KeyValuePair<string, string>(s.Value.Name, s.Value.Value));
+            }
+            else if (requestType == typeof(LaunchRequest))
+            {
+                commonModel.Request.Intent = "WelcomeIntent";
+            }
+            else if (requestType == typeof(SessionEndedRequest))
+            {
+                return null;
+            }
+
+            return commonModel;
+        }
+
+    }
+}
---
+++ Models/Common/CommonModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleEchoBot.Models.Common
+{
+
+    public class CommonModel
+    {
+        public CommonModel()
+        {
+            Id = "";
+            Session = new Session();
+            Request = new Request();
+            Response = new Response();
+        }
+
+        public string Id { get; set; }
+        internal Session Session { get; set; }
+        internal Request Request { get; set; }
+        internal Response Response { get; set; }
+    }
+
+    internal class Response
+    {
+        public string Text { get; set; }
+    }
+
+    internal class Request
+    {
+        public string Id { get; set; }
+        public string Intent { get; set; }
+        public List<KeyValuePair<string, string>> Parameters { get; set; }
+    }
+
+    internal class Session
+    {
+        public string Id { get; set; }
+    }
+}
